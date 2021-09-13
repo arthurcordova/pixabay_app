b933016dd5eb062fa31634bec450ce68c75ca68e
Arthur Cordova Stapassoli <arthur.stapassoli@gmail.com>
2021-09-13 09:16:06 -0300

    View types Adapter

--- app/src/main/java/com/arcanesecurity/pixabayapp/adapter/FeedVideoAdapter.kt
+++ app/src/main/java/com/arcanesecurity/pixabayapp/adapter/FeedVideoAdapter.kt
@@ -10,22 +10,37 @@ import com.arcanesecurity.pixabayapp.R
 import com.arcanesecurity.pixabayapp.databinding.FeedVideoItemBinding
 import com.arcanesecurity.pixabayapp.model.VideoConfig
 import com.bumptech.glide.Glide
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
-class FeedVideoAdapter : ListAdapter<VideoConfig, FeedVideoViewHolder>(VideosDiffCallback()) {
+const val NORMAL_VIEW = 0
+const val ADS_VIEW = 1
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeedVideoViewHolder {
+class FeedVideoAdapter : ListAdapter<VideoConfig, RecyclerView.ViewHolder>(VideosDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        if (viewType == ADS_VIEW) {
+            LayoutInflater.from(parent.context).inflate(R.layout.ads_item, parent, false).apply {
+                return AdsViewHolder(this)
+            }
+        }
         LayoutInflater.from(parent.context).inflate(R.layout.feed_video_item, parent, false).apply {
             return FeedVideoViewHolder(this)
         }
     }
 
-    override fun onBindViewHolder(holder: FeedVideoViewHolder, position: Int) {
-        getItem(position).let { video ->
-            holder.bind(video)
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (getItemViewType(position) == NORMAL_VIEW) {
+            holder as FeedVideoViewHolder
+            getItem(position).let { video ->
+                holder.bind(video)
+            }
+        } else {
+            holder as AdsViewHolder
         }
+    }
+
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position == 3) ADS_VIEW else NORMAL_VIEW
     }
 }
 
@@ -35,9 +50,11 @@ class FeedVideoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     fun bind(video: VideoConfig) {
         binding.textViewName.text = video.user
+        binding.textViewLikes.text = video.likes.toString()
         Glide.with(itemView).load(video.userImageURL).into(binding.imageViewAvatar)
         binding.videoView.setVideoURI(Uri.parse(video.videos.medium.url))
         binding.videoView.start()
+        binding.bottomBarInfo.visibility = View.GONE
         binding.videoView.setOnPreparedListener { mediaPlayer ->
             mediaPlayer.isLooping = true
             val videoRatio = mediaPlayer.videoWidth / mediaPlayer.videoHeight.toFloat()
@@ -48,7 +65,8 @@ class FeedVideoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
             } else {
                 binding.videoView.scaleY = 1f / scaleX
             }
-
+            binding.bottomBarInfo.visibility = View.VISIBLE
         }
+
     }
 }
